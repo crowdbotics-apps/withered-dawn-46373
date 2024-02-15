0b5b811207e0224bd47d92fc53871a99cf1aa693
Crowdbotics <team@crowdbotics.com>
2024-02-15 15:01:35 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-dawn-46373",
+  url: "https://withered-dawn-46373.botics.co",
+  api: "https://withered-dawn-46373.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
